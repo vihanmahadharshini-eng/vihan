866c213a8b9a4a96dd945878779c826f6d64b44c
vihanmahadharshini-eng <vihanmahadharshini@gmail.com>
2026-06-11 10:56:32 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,20 @@
-const CACHE_NAME = "vihan-v1";
+const CACHE_NAME = 'vihan-v2';
 
-const urlsToCache = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+self.addEventListener('install', event => {
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
   );
+  self.clients.claim();
 });
