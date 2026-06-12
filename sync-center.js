4107a0d58a3f10421fd3a75bfcc487d514aaf769
vihanmahadharshini-eng <vihanmahadharshini@gmail.com>
2026-06-12 10:36:19 +0530

    Update sync-center.js

--- sync-center.js
+++ sync-center.js
@@ -12,10 +12,14 @@ function createSyncButton() {
   btn.innerText = "Sync Center";
 
   btn.style.position = "fixed";
-  btn.style.bottom = "90px";
+  btn.style.bottom = "75px";
   btn.style.right = "20px";
   btn.style.zIndex = "9999";
   btn.style.padding = "10px 15px";
+  btn.style.background = "#2563EB";
+  btn.style.color = "white";
+  btn.style.border = "none";
+  btn.style.borderRadius = "10px";
 
   btn.addEventListener("click", function() {
     alert("Vihan Sync Center Coming Soon");
